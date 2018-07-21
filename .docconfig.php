9162c1ebf83304ff3d141c5db052846f291bd4a0
ryunosuke <ryunosuke.arima@gmail.com>
2018-07-21 20:32:24 +0900

    documentize 更新

--- .docconfig.php
+++ .docconfig.php
@@ -1,8 +1,16 @@
 <?php
 
-$rdi = new \RecursiveDirectoryIterator(__DIR__ . '/src', \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::CURRENT_AS_PATHNAME);
-$rii = new \RecursiveIteratorIterator($rdi, \RecursiveIteratorIterator::LEAVES_ONLY);
-$filelist = iterator_to_array($rii);
+$rdi = new \RecursiveDirectoryIterator(__DIR__ . '/src', \FilesystemIterator::SKIP_DOTS);
+$rii = new \RecursiveIteratorIterator($rdi, \RecursiveIteratorIterator::CHILD_FIRST);
+$filelist = [];
+foreach ($rii as $it) {
+    if (!$it->isDir()) {
+        $filelist[] = $it->getPathname();
+    }
+}
+
+$lock = json_decode(file_get_contents(__DIR__ . '/composer.lock'), true);
+$version = array_column($lock['packages'], 'version', 'name');
 
 return [
     'cachekey'   => md5(implode(',', array_map('md5_file', $filelist))),
@@ -10,7 +18,16 @@ return [
     'frontpage'  => 'ryunosuke\\dbml\\',
     'menusize'   => 35,
     'source-map' => [
-        '.*/Doctrine/DBAL/' => 'https://github.com/doctrine/dbal/blob/v2.7.1/lib/Doctrine/DBAL/',
-        '.*/'               => 'https://github.com/arima-ryunosuke/dbml/blob/master/src/dbml/',
+        '.*/Doctrine/Common/(Event.*)' => function ($m) use ($version) {
+            return "https://github.com/doctrine/event-manager/blob/{$version['doctrine/event-manager']}/lib/Doctrine/Common/" . $m[1];
+        },
+        '.*/Doctrine/Common/Cache/'    => function ($m) use ($version) {
+            return "https://github.com/doctrine/cache/blob/{$version['doctrine/cache']}/lib/Doctrine/Common/Cache/";
+        },
+        '.*/Doctrine/DBAL/'            => function ($m) use ($version) {
+            return "https://github.com/doctrine/dbal/blob/{$version['doctrine/dbal']}/lib/Doctrine/DBAL/";
+        },
+        '.*/dbml/'                     => 'https://github.com/arima-ryunosuke/dbml/blob/master/src/dbml/',
+        '.*'                           => '',
     ],
 ];
