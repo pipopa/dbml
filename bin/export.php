f831044c420f9a44d7f8e28793583ca262cbaa08
ryunosuke <ryunosuke.arima@gmail.com>
2019-07-14 23:03:28 +0900

    [composer] export
    - functions を使用されている関数だけ出力されるものとすべてのものに分離
    - 開発時は -dev を使用して、 push 前に build すれば使用されるものだけが吐き出される

--- bin/export.php
+++ bin/export.php
@@ -4,4 +4,6 @@ namespace ryunosuke\dbml;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-file_put_contents(__DIR__ . "/../src/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__));
+$directory = __DIR__ . "/../src";
+file_put_contents("$directory/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__, false, "$directory/dbml"));
+file_put_contents("$directory/functions-dev.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__));
