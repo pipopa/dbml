cf61e92741c2a60ffd52f848b2a2bebc7c9d068b
ryunosuke <ryunosuke.arima@gmail.com>
2019-09-10 20:47:12 +0900

    [composer] update
    - functions の構成を変更
      - dev ファイルは tests に追い出した
    - paml を使うようにするので symfony/yaml は不要

--- bin/export.php
+++ bin/export.php
@@ -4,6 +4,10 @@ namespace ryunosuke\dbml;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$directory = __DIR__ . "/../src";
-file_put_contents("$directory/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__, false, "$directory/dbml"));
-file_put_contents("$directory/functions-dev.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__));
+$src = __DIR__ . "/../src";
+$tst = __DIR__ . "/../tests";
+
+// 使用されているものだけ吐き出す（「プレーン・テキストとしてマーク」するとよい）
+file_put_contents("$src/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__, false, "$src/dbml"));
+// ただし、開発中に不便なので tests にすべて吐き出す
+file_put_contents("$tst/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__));
